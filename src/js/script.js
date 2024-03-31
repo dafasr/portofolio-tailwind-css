3d649cc77bc70a067487622b9c35fda744d91d12
dafasr <dafashanrizki2200@gmail.com>
2024-03-31 22:24:12 +0700

    Remove navbar menu toggle on hamburger click
    This commit removes the functionality that toggles the visibility of the navbar menu when the hamburger icon is clicked. The hamburger icon's appearance is still toggled with the 'hamburger-active' class.

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,4 @@ const hamburger = document.querySelector('#hamburger');
 
 hamburger.addEventListener('click', function () {
   this.classList.toggle('hamburger-active');
-  const navbarMenu = document.querySelector('.navbar__menu');
-  navbarMenu.classList.toggle('show');
 });
